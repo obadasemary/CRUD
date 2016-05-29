04a793855f6a33dc43def139857b6fec58497d16
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-05-29 15:52:00 +0300

    1.1
    Core Data
    Make CRUD System

--- CRUD/ViewController.swift
+++ CRUD/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var context: NSManagedObjectContext?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // ManagedObjectContext
+        context = CoreDataStackManager.sharedInstacne().managedObjectContext
+        
+        // new record
+//        let newUser = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: context!)
+//        
+//        newUser.setValue("Sara", forKey: "username")
+//        newUser.setValue("Sara249!", forKey: "password")
+//        newUser.setValue(21, forKey: "age")
+        
+        // saving to Core Data
+        do {
+            try context!.save()
+            
+            print("Successfully saved to the Core Data")
+        } catch {
+            // error 
+            print("error saving to the Core Data")
+        }
+        
+        fetch()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func fetch() {
+        
+        let request = NSFetchRequest(entityName: "User")
+        
+        do {
+            let results = try context?.executeFetchRequest(request)
+            
+            // print results nicely
+            
+            for result in results as! [NSManagedObject] {
+                
+                print(result.valueForKey("username")!)
+                print(result.valueForKey("password")!)
+                print(result.valueForKey("age")!)
+            }
+            
+        } catch {
+            print("Fetch Error")
+        }
     }
-
-
 }
 
